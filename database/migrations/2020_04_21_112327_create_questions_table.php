a92bc5a9d1528c9a2c9984acb93084dc84c940ec
ayomide2020 <adaramajataiwo@gmail.com>
2020-04-23 15:52:05 +0100

    Home page, Dashboard, Article's page implemented

---
+++ database/migrations/2020_04_21_112327_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->string('tag_ids');
+            $table->string('question');
+            $table->string('level')->default('Beginner');
+            // $table->integer('status')->comment('0 => deleted, 1 => active, 2 => duplicated');
+            $table->boolean('solved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.auth')
+
+@section('content')
+    <div id="app">
+        <dashboard-component></dashboard-component>
+    </div>
+@endsection
